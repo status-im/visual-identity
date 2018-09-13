b7111664b64f57c7114efec418b6452808af60cc
Barry Gitarts <bgitarts@gmail.com>
2018-09-13 16:57:59 -0400

    add get canvas state

--- app/components/draw/DrawingCanvas.js
+++ app/components/draw/DrawingCanvas.js
@@ -1,7 +1,8 @@
 import React, { PureComponent, Fragment } from 'react';
 import { CirclePicker } from 'react-color';
 import CanvasDraw from "react-canvas-draw";
-import { AppBar, Toolbar } from '@material-ui/core';
+import { AppBar, Toolbar, IconButton } from '@material-ui/core';
+import ZoomOutIcon from '@material-ui/icons/ZoomOut';
 
 
 class DrawingCanvas extends PureComponent {
@@ -14,15 +15,32 @@ class DrawingCanvas extends PureComponent {
     disabled: false
   }
 
-  render() {
+  zoomOut = () => {
+    const { canvasWidth, canvasHeight } = this.state;
+    const canvasState = this.saveableCanvas.getSaveData();
+    this.setState({
+      canvasWidth: canvasWidth * 1.25,
+      canvasHeight: canvasHeight * 1.25
+    })
+    console.log({canvasState})
+}
+
+render() {
     const { canvasWidth, canvasHeight, brushSize, brushColor } = this.state;
     return (
       <Fragment>
         <AppBar>
-          <Toolbar></Toolbar>
+          <Toolbar>
+            <IconButton
+              ref='zoom1'
+              onClick={this.zoomOut}>
+              <ZoomOutIcon/>
+            </IconButton>
+          </Toolbar>
         </AppBar>
         <CanvasDraw
           style={{ marginTop: '20%' }}
+          ref={canvasDraw => (this.saveableCanvas = canvasDraw)}
           canvasWidth={canvasWidth}
           canvasHeight={canvasHeight}
           brushSize={brushSize}
